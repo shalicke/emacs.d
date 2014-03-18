c2b88dfa98a8cd2ff6288782a5a688969ec8cc53
Sam Halicke <sam@halicke.com>
2014-03-18 15:07:24 -0500

    magit keymap, keychord fix, helm

--- kongming/kongming-bindings.el
+++ kongming/kongming-bindings.el
@@ -3,6 +3,9 @@
 ;; Global keybinds.
 ;;; Code:
 
+;; flycheck test
+(global-set-key (kbd "C-c C-e") 'flycheck-tip-cycle)
+
 ;; remap movement
 
 (global-set-key (kbd "M-j") 'next-line)
@@ -14,8 +17,9 @@
 (require 'key-chord)
 (key-chord-mode 1)
 
-(key-chord-define-global "//" 'isearch-forward-regexp)
-(key-chord-define-global "??" 'isearch-backward-regexp)
+;; (key-chord-define-global "/?" 'isearch-forward-regexp)
+(key-chord-define-global "??" 'isearch-forward-regexp)
+
 
 ;; windows
 
@@ -28,16 +32,32 @@
 ;;(global-set-key (kbd "C-x w") 'save-buffer-delete-frame)
 ;;(global-set-key (kbd "C-x C-k") 'save-buffer-kill-buffer)
 
-(global-set-key (kbd "C-f") nil)
-(global-set-key (kbd "C-f C-f") 'ace-jump-mode)
+
+(global-set-key (kbd "C-M--") 'text-scale-decrease)
+(global-set-key (kbd "C-M-+") 'text-scale-increas)
+
+(global-set-key (kbd "C-f f") 'ace-jump-mode)
 (global-set-key (kbd "C-f /") 'isearch-forward-regexp)
 (global-set-key (kbd "C-f ?") 'isearch-backward-regexp)
 
-;; find regexp in files matching current buffer's major mode (see functions.el)
+;; find regexp in files matching current buffer's major mode (see
+;; functions.el)
+;; TODO: fix this
 (global-set-key (kbd "C-f g") 'multi-occur-in-this-mode)
 
+(defvar kongming:magit-keymap nil)
+(define-prefix-command 'kongming:magit-keymap)
+(global-set-key (kbd "C-x C-g") 'kongming:magit-keymap)
+
+(define-key kongming:magit-keymap (kbd "s") 'magit-status)
+(define-key kongming:magit-keymap (kbd "c") 'magit-commit)
+(define-key kongming:magit-keymap (kbd "l") 'magit-log)
+
 ;; Helm.
-(global-set-key (kbd "C-c h") 'helm-mini)
+
+(global-set-key (kbd "C-c h h") 'helm-mini)
+(global-set-key (kbd "C-c h <SPC>") 'helm-all-mark-ring)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-c f") 'helm-for-files)
