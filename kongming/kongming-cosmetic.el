89a916eea703e27c836e7e3b9413852c008ca4de
Sam Halicke <sam@halicke.com>
2014-03-18 15:06:33 -0500

    popwin, fringe rearrangement, diminish.el

--- kongming/kongming-cosmetic.el
+++ kongming/kongming-cosmetic.el
@@ -3,19 +3,13 @@
 ;; cosmetic stuff -- fonts, theemes, colors, fringe, etc.
 ;;; Code:
 
-(when (window-system)
-  (require 'git-gutter-fringe+))
 
-(setq-default indicate-buffer-boundaries 'left)
-(setq-default indicate-empty-lines +1)
-
-(setq-default left-fringe-width 12)
-(setq-default right-fringe-width 0)
 
 ;; rainbow mode
 (rainbow-mode 1)
 
 ;; Line numbers
+(require 'linum)
 (global-linum-mode 1)
 (setq linum-format "%2d")
 
@@ -29,6 +23,37 @@
 
 ;; tooltips in echo area
 (tooltip-mode)
+
+;; pop window from bottom with popwin.
+(require 'popwin)
+(popwin-mode 1)
+(setq display-buffer-function 'popwin:display-buffer)
+
+(setq popwin:special-display-config
+      '(("*Help*"  :height 30)
+        ("*Completions*" :noselect t)
+        ("*Messages*" :noselect t :height 30)
+        ("*compilation*" :noselect t)
+        ("*Backtrace*" :height 30)
+        ("*Messages*" :height 30)
+        ("*Occur*" :noselect t)
+        ("*Ido Completions*" :noselect t :height 30)
+        ("*magit-commit*" :noselect t :height 40 :width 80 :stick t)
+        ("*magit-diff*" :noselect t :height 40 :width 80)
+        ("*magit-edit-log*" :noselect t :height 15 :width 80)
+        ("\\*magit\\.*" :regexp t :height 30 :width 80 :noselect)
+        ("\\*ansi-term\\*.*" :regexp t :height 30)
+        ("*shell*" :height 30)
+        ("*gists*" :height 30)
+        ("*sldb.*":regexp t :height 30)
+        ("*cider-error*" :height 30 :stick t)
+        ("*cider-doc*" :height 30 :stick t)
+        ("*cider-src*" :height 30 :stick t)
+        ("*cider-result*" :height 30 :stick t)
+        ("*cider-macroexpansion*" :height 30 :stick t)
+        ("*Compile-Log*" :height 30 :stick t)
+        ("*Deft*" :height 30 :stick t)
+        ("*git-gutter+-diff*" :height 15 :stick t)))
 
 ;; trim whitespace.
 ;; cribbed from emacs live
@@ -51,18 +76,31 @@
 ;; but enable the menubar.
 (if (fboundp 'menu-bar-mode) (menu-bar-mode +1))
 
-;; TODO: change this up color-wise. git-gutter has to work here as well. HARMONIZE!
-(if (fboundp 'fringe-mode) (fringe-mode 8))
+;; fringe stuff
+
+(setq-default indicate-buffer-boundaries 'left)
+(setq-default indicate-empty-lines +1)
+
+(setq-default left-fringe-width 12)
+(setq-default right-fringe-width 0)
+
+(if (fboundp 'fringe-mode) (fringe-mode 12))
+
+;; highlight differences
+(diff-hl-mode 1)
 
 ;; powerline
 (require 'powerline)
-(powerline-default-theme)
+(powerline-center-theme)
 
 ;; 24+ themes
+
+(setq solarized-high-contrast-mode-line t)
 (load-theme 'solarized-dark)
 
 ;; font setting from emacs-live
 
+;; for remove-if
 (require 'cl)
 
 (defun live-set-default-font (font-string)
@@ -75,7 +113,7 @@
   (cond
    ((member (window-system) '(x w32 ns))
     (add-to-list 'default-frame-alist (cons 'font font-string))
-    (set-default-font font-string t t))))
+    (set-frame-font font-string t t))))
 
 
 ;; TODO: Why is this necessary? It works, but, research.
@@ -86,10 +124,14 @@
    ((eq system-type 'darwin)
     (live-set-default-font font-string))))
 
-(live-set-default-darwin-font "PragmataPro-12")
-(set-frame-font "PragmataPro-12")
+(live-set-default-darwin-font "PragmataPro-14")
+
+;; (set-frame-font "PragmataPro-14")
 ;; no tabs
 (set-default 'indent-tabs-mode nil)
+
+(require 'diminish)
+(eval-after-load "filladapt" '(diminish 'filladapt-mode))
 
 (provide 'kongming-cosmetic)
 ;;; kongming-cosmetic.el ends here
