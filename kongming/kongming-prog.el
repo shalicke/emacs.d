4aeb1f385897413eaf7b873df628663cad459912
Sam Halicke <sam@halicke.com>
2014-03-24 11:00:03 -0700

    ocp-index and ocp-indent

--- kongming/kongming-prog.el
+++ kongming/kongming-prog.el
@@ -123,7 +123,14 @@
 
 ;; ocaml
 ;;(require 'kongming-ocaml)
-
+(autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+(add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
+(add-to-list 'load-path (concat
+     (replace-regexp-in-string "\n$" ""
+        (shell-command-to-string "opam config var share"))
+     "/emacs/site-lisp"))
+(require 'ocp-indent)
+(require 'ocp-index)
 ;; org
 ;;(require 'kongming-org)
 
